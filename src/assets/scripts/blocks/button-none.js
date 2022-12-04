ab55fb6c35760972ef5d26c47f9f292eb2a593d1
Toxa <suppertoha@yandex.ru>
2022-12-04 16:36:18 +0300

    tabel

--- src/assets/scripts/blocks/button-none.js
+++ src/assets/scripts/blocks/button-none.js
@@ -1,5 +1,30 @@
-const button = document.querySelector('.filter__show-more'); 
-const active_button = document.querySelector('.categories');
-button.addEventListener('click', function(){
-    active_button.classList.toggle('categories')
-})
+//const button = document.querySelector('.filter__show-more'); 
+//const active_button = document.querySelector('.categories');
+//button.addEventListener('click', function(){
+//    active_button.classList.toggle('categories')
+//})
+
+
+const catalogItems = document.querySelectorAll('.catalog-item');
+  if (!catalogItems) return;
+
+  catalogItems.forEach((catalogItem) => {
+      const btnOpenCatalog = catalogItem.firstElementChild.querySelector('.go-js-drop-catalog')
+     btnOpenCatalog.addEventListener('click', listOpen);
+
+     function listOpen(e) {
+        let listHeader = btnOpenCatalog.parentNode;
+        listHeader.classList.toggle('cat-header--active');
+        btnOpenCatalog.classList.toggle('cat-header__btn--active');
+
+        let subList = listHeader.closest('.catalog-item').querySelector('.sub-catalog');
+
+        subList.classList.toggle('active');
+
+        if (subList.classList.contains('active')) {
+           subList.style.maxHeight = subList.scrollHeight + "px";
+        } else {
+           subList.style.maxHeight = 0;
+        }
+     }
+  });
--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -16,91 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
     @@include('./blocks/page.js');
     @@include('./blocks/popup.js');
     @@include('./blocks/unit-block.js');
+    @@include('./blocks/modal-popup.js');
     @@include('./blocks/text.js');
     @@include('./blocks/slider.js');
     @@include('./blocks/button-none.js');
+    @@include('./blocks/table-header-scroll.js');
 
-
-    function throttle(func, ms) {
-        let isThrottled = false;
-        let savedArgs = null;
-        let savedThis = null;
-      
-        return function wrapper () {
-          if (isThrottled) {
-            savedArgs = arguments;
-            savedThis = this;
-            return;
-          }
-      
-          func.apply(this, arguments);
-          isThrottled = true;
-      
-          setTimeout(function() {
-            isThrottled = false;
-            
-            if (savedArgs) {
-              wrapper.apply(savedThis, savedArgs);
-              savedArgs = savedThis = null;
-            }
-          }, ms);
-        }
-      }
-
-    // window.addEventListener('scroll', throttle(function () {
-    //     const { scrollY } = window;
-    //     const headerHeight = document.querySelector('.header').offsetHeight
-    //     const element = document.querySelector('[data-sticke-element]') 
-    //     const wrapper = element.parentElement
-    //     const startOffset = wrapper.getBoundingClientRect().top + scrollY
-    //     const endOffset = wrapper.getBoundingClientRect().top + wrapper.clientHeight + scrollY - headerHeight
-
-    //     if (scrollY + headerHeight > startOffset) {
-    //         element.classList.add('fixed')
-    //         element.style.top = headerHeight + 'px'
-    //         wrapper.style.display = 'block'
-    //         wrapper.style.paddingTop = element.clientHeight + 'px'
-    //     } else {
-    //         element.classList.remove('fixed')
-    //         element.style.top = 0
-    //         wrapper.style.paddingTop = '0'
-    //     }
-
-    //     if (scrollY >= endOffset) {
-    //         element.classList.add('stopped')
-    //         element.style.top = wrapper.clientHeight + 'px'
-    //     } else {
-    //         element.classList.remove('stopped')
-    //     }
-    // }, 50))
-
-
-    window.addEventListener('scroll', function () {
-      const { scrollY } = window;
-      const headerHeight = document.querySelector('.header').offsetHeight
-      const element = document.querySelector('[data-sticke-element]') 
-      const wrapper = element.parentElement
-      const startOffset = wrapper.getBoundingClientRect().top + scrollY
-      const endOffset = wrapper.getBoundingClientRect().top + wrapper.clientHeight + scrollY - headerHeight
-
-      if (scrollY + headerHeight > startOffset) {
-          element.classList.add('fixed')
-          element.style.width = wrapper.clientWidth + 'px'
-          element.style.top = headerHeight + 'px'
-          wrapper.style.display = 'block'
-          wrapper.style.paddingTop = element.clientHeight + 'px'
-      } else {
-          element.classList.remove('fixed')
-          element.style.top = 0
-          element.style.width = 'auto'
-          wrapper.style.paddingTop = '0'
-      }
-
-      if (scrollY + headerHeight >= endOffset + element.clientHeight) {
-          element.classList.add('stopped')
-          element.style.top = wrapper.clientHeight + element.clientHeight + 'px'
-      } else {
-          element.classList.remove('stopped')
-      }
-  })
 });
