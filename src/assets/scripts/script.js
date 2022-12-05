e77bdfc8ae373e96c5e7405bfdb6fb2787a59900
Toxa <suppertoha@yandex.ru>
2022-12-05 11:58:28 +0300

    last

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -17,9 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
     @@include('./blocks/popup.js');
     @@include('./blocks/unit-block.js');
     @@include('./blocks/modal-popup.js');
-    @@include('./blocks/text.js');
     @@include('./blocks/slider.js');
     @@include('./blocks/button-none.js');
-    @@include('./blocks/table-header-scroll.js');
+  @@include('./blocks/table-header-scroll.js');
+  
+  
+    @@include('./blocks/text.js');
+  
 
 });
