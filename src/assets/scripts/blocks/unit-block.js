d64e9850039ab34cb2c31712d8572e67289939ad
Toxa <suppertoha@yandex.ru>
2022-12-13 11:08:19 +0300

    visivig-dark

--- src/assets/scripts/blocks/unit-block.js
+++ src/assets/scripts/blocks/unit-block.js
@@ -6,24 +6,62 @@
     // Unit Block
 
     const initUnitBlock = () => {
-      const unitBlocks = document.querySelectorAll('.js-unit-block');
+      // const unitBlocks = document.querySelectorAll('.js-unit-block');
     
+      // if (!unitBlocks) return;
 
-        if (!unitBlocks) return;
-        unitBlocks.forEach((unitBlock) => {
-            const unitBlockButton = unitBlock.querySelector('.js-unit-block__button');
-            const unitBlockContent = unitBlock.querySelector('.js-unit-block__content');
-            unitBlockContent.style.height = unitBlockContent.scrollHeight + 'px';
-            unitBlockButton.addEventListener('click', () => {
-                if (unitBlock.classList.contains('unit-block--closed')) {
-                    unitBlock.classList.remove('unit-block--closed');
-                    unitBlockContent.style.height = unitBlockContent.scrollHeight + 'px';
-                } else {
-                    unitBlock.classList.add('unit-block--closed');
-                    unitBlockContent.style.height = 0 + 'px';
-                }
-            });
-        });
+      // const handleUnitBlock = (unitBlock) => {
+      //   const unitBlockButton = unitBlock.querySelector('.js-unit-block__button');
+      //   const unitBlockContent = unitBlock.querySelector('.js-unit-block__content');
+        
+      //   unitBlockContent.style.height = unitBlockContent.scrollHeight + 'px';
+
+      //   const handleClick = () => {
+      //     if (unitBlock.classList.contains('unit-block--closed')) {
+      //       unitBlock.classList.remove('unit-block--closed');
+      //       unitBlockContent.style.height = unitBlockContent.scrollHeight + 'px';
+      //     } else {
+      //       unitBlock.classList.add('unit-block--closed');
+      //       unitBlockContent.style.height = 0 + 'px';
+      //     }
+      //   }
+      //   unitBlockButton.addEventListener('click', handleClick);
+      // }
+
+      const $unitBlock = $('.js-unit-block');
+      const $unitBlockButton = $('.js-unit-block__button');
+      const $unitBlockContent = $('.js-unit-block__content')
+
+      function handleUnitBlock () {
+        const $unitBlock = $(this).closest('.js-unit-block');
+        const $unitBlockContent = $unitBlock.find('.js-unit-block__content');
+        const contentHeight = `${$unitBlockContent.prop('scrollHeight')}px`
+
+        if ($unitBlock.hasClass('unit-block--closed')) {
+          $unitBlock.removeClass('unit-block--closed');
+          $unitBlockContent.css('height', contentHeight);
+        } else {
+          $unitBlock.addClass('unit-block--closed');
+          $unitBlockContent.css('height', 0);
+        }
+      }
+
+      function handleResize () {
+        if ($(window).width() > 1366) {
+          $unitBlockButton.off('click').on('click', handleUnitBlock)
+          $('#modal-accessories').removeAttr('style')
+        } else {
+          $unitBlockButton.off('click')
+          $unitBlock.removeClass('unit-block--closed')
+          $unitBlockContent.removeAttr('style')
+        }
+      }
+
+      $(window).on('resize', throttle(handleResize, 150))
+      
+      if ($(window).width() > 1366) {
+        $unitBlockButton.on('click', handleUnitBlock)
+      }
     };
 
 initUnitBlock();
@@ -51,4 +89,3 @@ function prompt() {
 }
 
 prompt()
-
